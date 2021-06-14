10a8c1c2b05f280ee184559405b8abe5ee1fda48
Harshad <hp1004032@gmail.com>
2021-06-14 11:42:49 +0530

    static files

--- myBlogSite/myBlogSite/settings.py
+++ myBlogSite/myBlogSite/settings.py
@@ -129,10 +129,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"

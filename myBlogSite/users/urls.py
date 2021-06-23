51466a43abf01298f10bd9671a3038aaec4dc9e5
Harshad <hp1004032@gmail.com>
2021-06-23 20:14:17 +0530

    blogwriters

--- myBlogSite/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-#from .views import UsersRegisterView,authenticate
-urlpatterns = [
-    path('',views.Register,name='users'),
-    path('/login',views.Login,name='login'),
-    ]
